18c6e8a06eb5a26893cdcb1b821065dac6e0c8f3
sktzofrenic <dan@danwins.com>
2016-05-03 16:52:10 -0400

    settings change

--- app/env_settings_example.py
+++ app/env_settings_example.py
@@ -14,10 +14,10 @@ SECRET_KEY = '\xb9\x8d\xb5\xc2\xc4Q\xe7\x8ej\xe0\x05\xf3\xa3kp\x99l\xe7\xf2i\x00
 SQLALCHEMY_DATABASE_URI = 'sqlite:///../app.sqlite'
 
 # Flask-Mail settings
-MAIL_USERNAME = 'email@example.com'
-MAIL_PASSWORD = 'password'
+MAIL_USERNAME = 'postmaster@sandboxe186d9e577054347ba3c6c6dd52932cc.mailgun.org'
+MAIL_PASSWORD = '7a3faa2e7ad6212219036029394e84f6'
 MAIL_DEFAULT_SENDER = '"AppName" <noreply@example.com>'
-MAIL_SERVER = 'MAIL_SERVER', 'smtp.gmail.com'
+MAIL_SERVER = 'MAIL_SERVER', 'smtp.mailgun.org'
 MAIL_PORT = 465
 MAIL_USE_SSL = True
 MAIL_USE_TLS = False
